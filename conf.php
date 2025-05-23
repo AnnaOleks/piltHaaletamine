93e9257983f6d229b2646b0f6aad9d274f427735
Anna Oleks <annaoleks88@gmail.com>
2025-05-23 18:34:46 +0300

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php
-$kasutaja="d133853_annaoleks";
-$parool="DorisOceana1323";
-$andmebaas="d133853_anna";
-$srverinimi="d133853.mysql.zonevs.eu";
+$kasutaja="___________";
+$parool="___________";
+$andmebaas="___________";
+$srverinimi="___________";
 
 $yhendus=new mysqli($srverinimi,$kasutaja,$parool,$andmebaas);
 $yhendus->set_charset("utf8");
